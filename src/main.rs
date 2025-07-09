28fe744f472cba3a77ec1294e3278c7d4d0f27a0
Cherry Ramatis <cherry.ramatis@icloud.com>
2025-07-09 14:34:34 -0300

    feat: Add a basic REPL functionality for the lexer

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,35 @@
 mod lexer;
 mod token;
 
+use std::io::{stdin, stdout, Write};
+
+const PROMPT: &str = ">> ";
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let mut line = String::new();
+
+        print!("{}", PROMPT);
+
+        let _ = stdout().flush();
+        stdin()
+            .read_line(&mut line)
+            .expect("Please type a valid string");
+
+        match line.trim() {
+            "exit" => {
+                break;
+            }
+            _ => {
+                let mut lexer = lexer::Lexer::new(line);
+
+                let mut token = lexer.next_token();
+
+                while token != token::Token::Eof {
+                    println!("{:?}", token);
+                    token = lexer.next_token();
+                }
+            }
+        };
+    }
 }
